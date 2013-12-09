15b98da5c19d9c096f2e9ecb0d6e36faa26c1fe7
lesiak <lesiak@outlook.com>
2013-12-09 12:20:59 +0100

    Selected row in decks table

--- FlashCardsNew/web/decks_table.dart
+++ FlashCardsNew/web/decks_table.dart
@@ -3,14 +3,18 @@ import 'dart:html';
 
 @CustomTag('decks-table')
 class DecksTableElement extends PolymerElement {
+  
   @published List<String> items;
+  
+  @observable String selectedName;
   
   DecksTableElement.created() : super.created();
   
   void wordFileClicked(Event e, var detail, Element target) {
     String deckName =  target.attributes['data-msg'];
     //message = target.attributes['data-msg'];
-    print(deckName);
+    //print(deckName);
+    selectedName = deckName;
     dispatchEvent(new CustomEvent('decknameclicked', detail: deckName)); 
   }
   
@@ -19,4 +23,16 @@ class DecksTableElement extends PolymerElement {
   }*/
   
   bool get applyAuthorStyles => true;
+  
+  String getCss(String wordFile, String _selectedNameTrigger) {
+    print("getCss" + wordFile);
+    if (wordFile == selectedName) {
+      return "selectedTableRow";
+      
+    }
+    
+   // else {
+      return "";
+    //}
+  }
 }
