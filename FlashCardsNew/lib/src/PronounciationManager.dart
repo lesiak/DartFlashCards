d021c3c3caf04bbc43c0cb1bbcb4cd08e002969f
lesiak <lesiak@outlook.com>
2014-04-14 16:40:21 +0200

    Some words

--- FlashCardsNew/lib/src/PronounciationManager.dart
+++ FlashCardsNew/lib/src/PronounciationManager.dart
@@ -10,7 +10,10 @@ class PronounciationManager {
   
   static final String forvoKey="ca19d7cd6c0d10ed257b2d23960933ee";
   
-  static Set<String> favoriteUsers = new Set.from(['ssoonkimi', 'x_WoofyWoo_x', 'floridagirl']);
+  static Set<String> favoriteUsers = new Set.from(['ssoonkimi', 
+                                                   'x_WoofyWoo_x', 
+                                                   'floridagirl',
+                                                   'guijaran2']);
   
   static Set<String> bannedUsers = new Set.from(['jcung']);
   
