697fd9e8141972e721263e542e0dcbc038817796
lesiak <lesiak@outlook.com>
2014-04-14 15:08:45 +0200

    Updated dictionary to more languages

--- FlashCardsNew/web/dictionary_panel.dart
+++ FlashCardsNew/web/dictionary_panel.dart
@@ -5,9 +5,15 @@ import 'package:FlashCardsNew/flashcards_core.dart';
 
 @CustomTag('dictionary-panel')
 class DictionaryPanelElement extends PolymerElement {
-     
+  
   @observable String searchTerm;
   
+  @published String primaryLang; 
+    
+  @published String secondaryLang;
+  
+  @published String thirdLang;
+         
   @published List<String> deckNames = toObservable([]);
   
   @published List<DictionaryTableRow> allCards = toObservable([]);
@@ -32,7 +38,8 @@ class DictionaryPanelElement extends PolymerElement {
     Future.forEach(deckNames, 
         (wordfile) => engine.loadDeckFile('./wordfiles/$wordfile.json')
         .then((deckCards) {
-          var cardRows = deckCards.map((card) => new DictionaryTableRow(card, wordfile));
+          var cardRows = deckCards.map((card) => 
+              new DictionaryTableRow(card, wordfile, primaryLang, secondaryLang, thirdLang));
           cards.addAll(cardRows);
         } , onError: (e, st) => print('Cannot read $wordfile, $e, $st')))
       .then((_) {
@@ -58,27 +65,71 @@ class DictionaryPanelElement extends PolymerElement {
   }
   
   bool isCardMatching(DictionaryTableRow row) {
-    var searchRegex = new RegExp(searchTerm, caseSensitive: false);
-    bool engVerbMatch = false;
-    if (row.card.en.startsWith("to ")) {
-      engVerbMatch = row.card.en.substring(3).startsWith(searchRegex);              
-    } 
-    return row.card.en.startsWith(searchRegex)
-        || engVerbMatch
-        || row.card.fi.startsWith(searchRegex)
-        || row.card.ko.startsWith(searchRegex)
-        || row.card.fr.startsWith(searchRegex);
+    var searchRegex = new RegExp(searchTerm, caseSensitive: false);     
+    return isCardMatchingLang(row.card, "en", searchRegex)
+        || isCardMatchingLang(row.card, primaryLang, searchRegex)
+        || isCardMatchingLang(row.card, secondaryLang, searchRegex)
+        || isCardMatchingLang(row.card, thirdLang, searchRegex);
   }
+  
+  static bool isCardMatchingLang(Card card, String langName, RegExp searchRegex) {
+    String cardValue = card.getValueForLang(langName);
+    if (cardValue == null) {
+      return false;
+    } else {
+      String sanitizedWord = sanitizeWordForMatching(cardValue, langName);      
+      return sanitizedWord.startsWith(searchRegex);
+    }    
+  }
+  
+  static String sanitizeWordForMatching(String word, String langName) {
+    if (langName == "en" && word.startsWith("to ")) {
+      return word.substring(3);
+    } else if (langName == "es" && (word.startsWith('(el) ') || word.startsWith('(la) '))) {
+      return word.substring(5);            
+    } else {
+      return word;
+    } 
+  }
+  
+  
    
   bool get applyAuthorStyles => true;
+  
+  String getLangName(String lang) {
+      if (lang == "ko") {
+        return "Korean";
+      } else if (lang == "fi") {
+        return "Finnish";      
+      } else if (lang == "fr") {
+        return "French";      
+      } else if (lang == "hu") {
+        return "Hungarian";      
+      } else if (lang == "zh") {
+        return "Chinese";      
+      } else if (lang == "es") {
+        return "Spanish";      
+      } else if (lang==null) {
+        return "null";
+      }
+      return "unknown";
+    }
     
 }
 
 class DictionaryTableRow extends Object with Observable {
   @observable Card card;
+  
+  @observable String first;
+  @observable String second;
+  @observable String third;
   @observable String deckName;
   
   
-  DictionaryTableRow(this.card, this.deckName);
+  DictionaryTableRow(this.card, this.deckName, String primLang, String secLang, String thirdLang) {
+    this.first = card.getValueForLang(primLang);
+    this.second = card.getValueForLang(secLang);
+    this.third = card.getValueForLang(thirdLang);
+  }
   
 }
