b609814153c3101c952702e70acbcd53ec287c5c
lesiak <lesiak@outlook.com>
2014-10-24 10:08:55 +0200

    Fixed imports

--- flashcardsnew/lib/flashcards_core.dart
+++ flashcardsnew/lib/flashcards_core.dart
@@ -3,10 +3,10 @@ library flashcards_core;
 import 'dart:html';
 import 'dart:async';
 import 'dart:convert' show JSON;
-import 'package:polymer/polymer.dart';
 
 part 'src/Card.dart';
 part 'src/CardScore.dart';
+part 'src/CardWithScore.dart';
 part 'src/ResultStore.dart';
 part 'src/DeckLoader.dart';
 part 'src/Engine.dart';
